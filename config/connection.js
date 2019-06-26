ed9d10e10578cc535663c48442b8ce72621fa9df
Armando Morales <amorales944@gmail.com>
2019-06-26 11:36:47 -0500

    Making connections route

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 var mysql = require("mysql");
+var connection;
 // ************************************************************
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -9,7 +12,5 @@ var connection = mysql.createConnection({
     database: "../db/TaconMadre_db"
   });
   
-  // connection.connect(function(err) {
-  //   if (err) throw err;
-  //   // runSearch();
-  // });
+  connection.connect();
+  module.exports = connection;
